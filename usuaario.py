ab8718413c00a52e39e6978b86b541d67a8eff05
Python <dumwebtecnologia@gmail.com>
2023-11-28 10:01:11 -0300

    commit do tio

---
+++ usuaario.py
@@ -0,0 +1,10 @@
+# Programa que diz bom dia ao usuaruio
+
+# Apresetação
+print('\n\t\t\t -- Dia Bom ao Usuario -- \n')
+
+# Entrada
+nome = input('Informe o seu nome: ')
+
+# Saida
+print('Bom dia ' + '.')
